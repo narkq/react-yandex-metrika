fdd98d794b5e13dc2daa2863979ead17414bcb52
Alex Sladkov <sladkov@gmail.com>
2015-07-24 17:47:20 +0300

    implementation that actually works

--- src/index.js
+++ src/index.js
@@ -8,39 +8,44 @@ var isBrowser = typeof window !== 'undefined';
 var c = 'yandex_metrika_callbacks';
 var a = 'yandex_metrika_accounts';
 
-if (isBrowser) {
-    window[c] = window[c] || [];
-    window[a] = window[a] || [];
+function ymProxy(methodName, ...args) {
+    window[a].forEach(id => {
+        try {
+            window[`yaCounter${id}`][methodName](...args);
+        } catch(ex) {
+            console.warn(ex);
+        }
+    });
 }
 
-function ym(methodName, ...args) {
+function ymAsyncProxy(...args) {
+    if (window[c]) {
+        window[c].push(() => ymProxy(...args));
+    } else {
+        ymProxy(...args);
+    }
+}
+
+function ym(...args) {
     if (isBrowser) {
-        window[c].push(() => {
-            window[a].forEach(id => {
-                try {
-                    window[`yaCounter${id}`][methodName](...args);
-                } catch(ex) {
-                    console.warn(ex);
-                }
-            });
-        });
+        ymAsyncProxy(...args);
     }
 }
 
 ym.Initializer = component;
 
-ym.initAccount = function initAccount(id, options = {}) {
-    if (isBrowser) {
-        let defaultOptions = {
-            id,
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true,
-            webvisor: true
-        };
+ymAsyncProxy.init = function init(accounts, options = {}) {
+    window[a] = window[a].concat(accounts);
+    window[c].push(() => {
+        window[a].forEach(id => {
+            let defaultOptions = {
+                id,
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                webvisor: true
+            };
 
-        window[a].push(id);
-        window[c].push(() => {
             try {
                 window[`yaCounter${id}`] = new Ya.Metrika(
                     Object.assign(defaultOptions, options)
@@ -49,7 +54,13 @@ ym.initAccount = function initAccount(id, options = {}) {
                 console.warn(ex);
             }
         });
-    }
+    });
 };
+
+if (isBrowser) {
+    window[c] = window[c] || [];
+    window[a] = window[a] || [];
+    window.ym = ymAsyncProxy;
+}
 
 export default ym;
